b5ef3ffe0b179e9adc8ea7464bbf5da9d00e39e6
stibu81 <slanz1137@gmail.com>
2024-05-24 22:50:10 +0200

    don't use devtools

--- tests/testdata/create_testdata.R
+++ tests/testdata/create_testdata.R
@@ -1,11 +1,11 @@
 # Generate test database from Firefox history
-library(devtools)
+library(testthat)
 library(lubridate)
 library(browserhistory)
 
 generate_testdata(
   root_dir = "~/snap/firefox/common/.mozilla/firefox/",
-  output_dir = package_file("tests", "testdata", "firefox"),
+  output_dir = test_path("..", "testdata", "firefox"),
   start_time = as.POSIXct("2024-05-24 17:42:00"),
   end_time = as.POSIXct("2024-05-24 17:46:00")
 )
