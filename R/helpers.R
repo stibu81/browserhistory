822a017a9b454c812e718c78d0d58c8a859da3c4
stibu81 <slanz1137@gmail.com>
2024-05-27 19:03:15 +0200

    explicitly set origin in firefox_ts_to_posix()

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,6 @@
 # convert timestamps from Firefox history to POSIX and vice versa
 firefox_ts_to_posix <- function(ts, tz = "") {
-  as.POSIXct(ts/1e6, tz = tz)
+  as.POSIXct(ts/1e6, tz = tz, origin = "1970-01-01 00:00:00")
 }
 
 posix_to_firefox_ts <- function(ts) {
